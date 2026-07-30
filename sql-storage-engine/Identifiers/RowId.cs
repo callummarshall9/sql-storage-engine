cd5f2c282d63e6efa7288d6f3a8ac1bcc58832cd
Callum <callummarshall9@googlemail.com>
2026-07-30 14:58:47 +0100

    Another formatting fix

--- sql-storage-engine/Identifiers/RowId.cs
+++ sql-storage-engine/Identifiers/RowId.cs
@@ -3,5 +3,5 @@ namespace sql_storage_engine.Identifiers;
 public readonly record struct RowId(PageId PageId, SlotId SlotId, SlotGeneration SlotGeneration)
 {
     public override string ToString()
-        => "PageId:" + PageId + ":SlotId:" + SlotId + ":SlotGeneration:" + SlotGeneration;
+        => $"{PageId}{SlotId}{SlotGeneration}";
 }
